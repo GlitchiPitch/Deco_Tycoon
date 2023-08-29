caee5c486b6dce42330526450b8ad5307106b9f1
GlitchiPitch <141921165+GlitchiPitch@users.noreply.github.com>
2023-08-29 19:58:52 +0500

    update

--- src/server/Modules/CreateBuild.lua
+++ src/server/Modules/CreateBuild.lua
@@ -1,4 +1,5 @@
 local ServerStorage = game:GetService('ServerStorage')
+local CollectionService = game:GetService('CollectionService')
 local Furniture = require(ServerStorage.Furniture)
 
 
@@ -6,15 +7,18 @@ local Build = {}
 
 Build.__index = Build
 
-function Build.new(buildFolder)
+function Build.new(buildFolder) -- build
     local self = setmetatable({}, Build)
     
     -- self.Build = 
     self.Parent = buildFolder
     self.CFrame = buildFolder.baseplate.CFrame
+    
+    self.Model = self:CreateBuild()  --build
+    self.Furniture = self:GetFurniture()
+    
 
-    self.Furniture = self:CreateFurniture()
-    -- create build with different floors, wall color and material
+
     -- create a function which are making square build and wall must be created of many parts
     -- furnitures templates must be include into the build and they are created random
     -- every parts of build must has a tag : "Destroyable"
@@ -22,15 +26,35 @@ function Build.new(buildFolder)
     return self
 end
 
+function Build:CreateBuild()
+    local build = Instance.new('Model')
+    -- create build with different floors, wall color and material
+    local furnitureAttachmentList = Instance.new('Folder')
+    furnitureAttachmentList.Parent = build
+    for index = 1,math.random(5) do
+        local furnitureAttachment = Instance.new('Part')
+        furnitureAttachment.Size = Vector3.new(.5,.5,.5)
+        furnitureAttachment.Anchored = true
+        -- furnitureAttachment.CFrame = 
+        furnitureAttachment.Parent = furnitureAttachmentList
+    end
+    return build
+end
+
 function Build:PrepareBuild()
-    
+    for _, item in pairs(self.Model:GetDescendants()) do
+        if item:IsA('Part') or item:IsA('UnionOperation') then
+            -- возможно сделать список и там их указывать через индекс
+            CollectionService:AddTag('Destroy')
+        end
+    end
 end
 
 function getCFrameForFurniture(baseplateCFrame)
     -- return random cframe of baseplateCFrame
 end
 
-function Build:CreateFurniture()
+function Build:GetFurniture()
 
     local furnitureFolder = Instance.new('Folder')
     furnitureFolder.Name = 'Furniture'
